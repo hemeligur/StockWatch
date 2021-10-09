bb7e5e1380e29dbe87b3974593ed5ac4c13d02c0
Guilherme F. A. Souza <guifas3000@gmail.com>
2021-10-09 17:06:24 -0300

    registering the models

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Stock, StockWatch
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Stock)
+admin.site.register(StockWatch)
