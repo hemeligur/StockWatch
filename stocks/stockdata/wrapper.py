63c25d0599672ed8140f6b574cdb2c026ec91482
Guilherme F. A. Souza <guifas3000@gmail.com>
2021-10-13 23:46:52 -0300

    Exposed the valid API periods and intervals as global variables

--- stocks/stockdata/wrapper.py
+++ stocks/stockdata/wrapper.py
@@ -3,22 +3,22 @@ import yfinance as yf
 
 # https://www.guiainvest.com.br/lista-acoes/default.aspx?listaacaopesquisa=petrobras
 
+API_VALID_INTERVALS = ["1m", "2m", "5m", "15m", "30m", "90m",
+                       "60m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
+API_VALID_PERIODS = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+
 
 class StockData():
     ''' A simple interface for the yahoo finance API to decouple it from the rest of the system '''
 
     def __init__(self, code):
-        self.VALID_INTERVALS = ["1m", "2m", "5m", "15m", "30m", "90m",
-                                "60m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
-        self.VALID_PERIODS = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
-
         self.code = code
         self.stock_data = yf.Ticker(self.code + ".SA")
 
     def _valid_interval(self, period, interval):
         try:
-            interval_idx = self.VALID_INTERVALS.index(interval)
-            period_idx = self.VALID_PERIODS.index(period)
+            interval_idx = API_VALID_INTERVALS.index(interval)
+            period_idx = API_VALID_PERIODS.index(period)
 
             error_message = (f"For interval {interval},"
                              f" the requested period must be within the last %d %s.")
@@ -50,7 +50,7 @@ class StockData():
             return None
 
     def get_last_price(self):
-        hist = self.get_history(period=self.VALID_PERIODS[0], interval=self.VALID_INTERVALS[0])
+        hist = self.get_history(period=API_VALID_PERIODS[0], interval=API_VALID_INTERVALS[0])
 
         return hist['Close'][0]
 
