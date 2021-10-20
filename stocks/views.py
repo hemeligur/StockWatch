8f2b9eb5fb53d8e504f5e7aefe9cc567b27e50fc
Guilherme F. A. Souza <guifas3000@gmail.com>
2021-10-20 02:49:58 -0300

    Code refactoring

--- stocks/views.py
+++ stocks/views.py
@@ -20,6 +20,7 @@ class StockSearchView(TemplateView):
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
         if query:
+            query = query.upper()
             return redirect('stocks:stock_detail', pk=query)
         else:
             return super().get(self, request, *args, **kwargs)
@@ -71,8 +72,13 @@ class StockDetailView(DetailView):
         if self.stock_data is None:
             self.stock_data = StockData(context['stock'].code)
         hist = self.stock_data.format_to_chart(self.stock_data.get_history())
-        context['chart_data'] = json.dumps(hist)
-        context['chart_title'] = json.dumps("Histórico dos últimos 6 meses")
-        context['chart_h'] = 800
-        context['chart_w'] = 2000
+
+        chart_config = {
+            'data': json.dumps(hist),
+            'title': json.dumps("Histórico dos últimos 6 meses"),
+            'height': 800,
+            'width': 2000
+        }
+
+        context['chart_config'] = chart_config
         return context
