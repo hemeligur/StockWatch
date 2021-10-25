bc34a770aa7b78637f69dd7651c2a4c8a25f8f19
Guilherme F. A. Souza <guifas3000@gmail.com>
2021-10-25 01:02:22 -0300

    User password reset

--- stockwatch/urls.py
+++ stockwatch/urls.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from stocks.views import LandingPageView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from watchers.views import SignUpView
 
 urlpatterns = [
@@ -11,6 +18,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignUpView.as_view(), name="signup"),
+    path('reset-password/', PasswordResetView.as_view(), name="reset_password"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path('stocks/', include("stocks.urls", namespace="stocks")),
     path('watchers/', include("watchers.urls", namespace="watchers"))
 ]
